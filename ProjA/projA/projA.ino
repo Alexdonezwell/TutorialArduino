5858b199785a71d742478662d9518f399ba1fa67
Dzung Nguyen <dzungng89@gmail.com>
2017-02-10 19:12:30 -0600

    Tutorial for arduino

---
+++ ProjA/projA/projA.ino
@@ -0,0 +1,27 @@
+#include <RFduinoBLE.h>
+/*
+  Piezoelectric sensors between GND and pin GPIO1
+  Sampling rate is 20Hz
+ */
+const int sensorPin = 1;
+int sensorValue = 0;  // variable to store the value coming from the sensor
+long clockTime = 0;
+
+void setup() {
+  pinMode(sensorPin, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the sensor at GPIO1
+  sensorValue = analogRead(sensorPin);
+  clockTime = millis();
+
+  // send data in CSV format
+  Serial.print(clockTime);
+  Serial.print(",");
+  Serial.println(sensorValue);
+
+  // sampling frequency 20 Hz
+  delay(50);     
+}
---
+++ ProjB/projB/projB.ino
@@ -0,0 +1,36 @@
+#include <RFduinoBLE.h>
+
+boolean connectFlag = true;
+
+const int sensorPin = 1;
+uint32_t sensorValue;
+uint32_t txBuf[5]; // since Bluetooth LE max size is 20 bytes
+
+void setup() {  
+  pinMode(sensorPin, INPUT);
+  
+  // start the BLE stack
+  RFduinoBLE.deviceName = "andrey"; // change name here to avoid conflict
+  RFduinoBLE.advertisementData = "piezoelectric";
+  RFduinoBLE.txPowerLevel = -4; 
+  RFduinoBLE.begin();
+
+  delay(1000);
+}
+
+void RFduinoBLE_onConnect() {
+  connectFlag = true;
+}
+
+void RFduinoBLE_onDisconnect() {
+  connectFlag = false;
+}
+
+void loop() {
+  if (connectFlag) {
+    RFduino_ULPDelay( SECONDS(0.01) );
+    sensorValue = analogRead(sensorPin);
+    txBuf[0] = sensorValue;
+    RFduinoBLE.send((char *)txBuf, 20);
+  }
+}
