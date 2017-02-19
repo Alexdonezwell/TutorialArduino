af4b80d4e3bdd4bb6deac17cb7de85e4d4a96cec
Dzung Nguyen <dzungng89@gmail.com>
2017-02-19 02:43:29 -0600

    Add timer tool

---
+++ ProjB/timerTool/experiment.js
@@ -0,0 +1,68 @@
+var experiment = [
+    {
+        action: 'Get ready',
+        secs: 10 },
+    {   
+        action: 'Eat soup with spoon',
+        secs: 120 }, 
+    {   
+        action: 'Rest',
+        secs: 30 }, 
+    {   
+        action: 'Eat bread with hand',
+        secs: 120 }, 
+    {   
+        action: 'Rest',
+        secs: 30 },    
+    {   
+        action: 'Drink water from a cup',
+        secs: 120 }, 
+    {  
+        action: 'Rest',
+        secs: 30 },
+    {   
+        action: 'Eat salad',
+        secs: 120 },
+    {   
+        action: 'Rest',
+        secs: 30 }, 
+    {
+        action: 'Drink water using straw',
+        secs: 120
+    },
+    {
+        action: 'Rest',
+        secs: 30
+    },
+    {
+        action: 'Eat chicken with fork and knife',
+        secs: 120
+    },
+    {
+        action: 'Rest',
+        secs: 30
+    },
+    {
+        action: 'Make a phone call for 2 mins',
+        secs: 120
+    },
+    {
+        action: 'Rest',
+        secs: 30
+    },
+    {
+        action: 'Eat chips',
+        secs: 120
+    },
+    {
+        action: 'Rest',
+        secs: 30
+    },   {   
+        action: 'Style and brush your hair in front of the laptop',
+        secs: 120 
+    },
+    {
+        action: 'Rest',
+        secs: 30
+    }
+]
